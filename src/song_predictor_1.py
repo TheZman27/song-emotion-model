3192b4ae166cbeb69b63231b7b003e9e50d12977
Zaq Holdsworth <62485420+TheZman27@users.noreply.github.com>
2026-03-18 15:16:14 -0600

    Create files for final product

---
+++ src/song_predictor_1.py
@@ -0,0 +1,47 @@
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.multioutput import MultiOutputRegressor
+
+
+def get_song_predictor() -> MultiOutputRegressor:
+    """Returns a trained sklearn.MultiOutputRegressor model on our survey data. 
+    The model inputs are:
+        release_year, duration_ms.x, explicit, 
+        mode, speechiness, acousticness,
+        instrumentalness, liveness, valence,
+        tempo, time_signature
+    The model outputs are
+        'delta: sad to happy',
+        'delta: calm to energetic',
+        'delta: relaxed to stressed'
+    """
+    df = pandas.read_csv('../out/merged_data.csv')
+    X = df.loc[
+        :,
+        [
+            'release_year', 'duration_ms.x', 'explicit', 
+            'mode', 'speechiness', 'acousticness',
+            'instrumentalness', 'liveness', 'valence',
+            'tempo', 'time_signature'
+        ]
+    ] 
+    Y = df.loc[
+        :,
+        [
+            'delta: sad to happy',
+            'delta: calm to energetic',
+            'delta: relaxed to stressed'
+        ]
+    ] 
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+
+    base_estimator = linear_model.LinearRegression()
+    model = MultiOutputRegressor(base_estimator)
+    model.fit(X, Y)
+
+    print(f"Training score (R^2) {model.score(X, Y)}")
+
+    return model
