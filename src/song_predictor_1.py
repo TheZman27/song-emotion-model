46e92096ff5d77e448794244dc91a56c9c144888
Zaq Holdsworth <62485420+TheZman27@users.noreply.github.com>
2026-03-26 12:29:47 -0600

    Add notebook for plotting the predictions and survey data

--- src/song_predictor_1.py
+++ src/song_predictor_1.py
@@ -4,7 +4,7 @@ from sklearn import linear_model
 from sklearn.multioutput import MultiOutputRegressor
 
 
-def get_song_predictor() -> MultiOutputRegressor:
+def get_song_predictor(file_address='./out/merged_data.csv') -> MultiOutputRegressor:
     """Returns a trained sklearn.MultiOutputRegressor model on our survey data. 
     The model inputs are:
         release_year, duration_ms.x, explicit, 
@@ -16,7 +16,7 @@ def get_song_predictor() -> MultiOutputRegressor:
         'delta: calm to energetic',
         'delta: relaxed to stressed'
     """
-    df = pandas.read_csv('./out/merged_data.csv')
+    df = pandas.read_csv(file_address)
     X = df.loc[
         :,
         [
